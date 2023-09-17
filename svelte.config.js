30ec9df65836f90ad2d6b14f77aab9940542adc1
FrancescoSardo <francescosardo91@gmail.com>
2023-09-17 20:18:09 +0200

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-/* import adapter from '@sveltejs/adapter-auto'; */
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-static';
+/* import adapter from '@sveltejs/adapter-static'; */
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,9 +12,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
+		/* paths: {
 			base: '/CartoleriaMietto',
-		}
+		} */
 	}	
 };
 
